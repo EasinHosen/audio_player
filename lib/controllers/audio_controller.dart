8a32bcafaeee70407cb98ec8b6c8e332aea7ae02
Easin Md. Hosen <easinhosen5@gmail.com>
2023-09-12 21:12:12 +0600

    feat: query initiated on controller

--- lib/controllers/audio_controller.dart
+++ lib/controllers/audio_controller.dart
@@ -1,3 +1,34 @@
+import 'package:audio_player/utils/global_helpers.dart';
 import 'package:get/get.dart';
+import 'package:on_audio_query/on_audio_query.dart';
 
-class AudioController extends GetxController {}
+class AudioController extends GetxController {
+  RxList<SongModel> audios = <SongModel>[].obs;
+  RxBool hasPermission = false.obs;
+  RxBool songListLoading = false.obs;
+  final OnAudioQuery _audioQuery = OnAudioQuery();
+
+  @override
+  onInit() {
+    checkAndReqPermission();
+    super.onInit();
+  }
+
+  getSongs() async {
+    songListLoading(true);
+    final audioList = await _audioQuery.querySongs();
+    audios.value = audioList;
+    songListLoading(false);
+
+    loggerDebug(audios.length, 'number of audio');
+  }
+
+  checkAndReqPermission() async {
+    hasPermission(await _audioQuery.checkAndRequest());
+    if (hasPermission.value) {
+      getSongs();
+    } else {
+      loggerDebug(hasPermission.value, 'Permission');
+    }
+  }
+}
