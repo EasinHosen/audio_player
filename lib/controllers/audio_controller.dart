e4f8864de2f79d6cb036b79ebc64575f25890f0b
Easin Md. Hosen <easinhosen5@gmail.com>
2023-09-12 21:31:17 +0600

    feat: audio list showed

--- lib/controllers/audio_controller.dart
+++ lib/controllers/audio_controller.dart
@@ -3,7 +3,10 @@ import 'package:get/get.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 
 class AudioController extends GetxController {
+  static AudioController get to => Get.find();
+
   RxList<SongModel> audios = <SongModel>[].obs;
+  // Rx<SongModel> nowPlaying = SongModel(_info).obs;
   RxBool hasPermission = false.obs;
   RxBool songListLoading = false.obs;
   final OnAudioQuery _audioQuery = OnAudioQuery();
--- lib/views/pages/audio_list_page/audio_list_screen.dart
+++ lib/views/pages/audio_list_page/audio_list_screen.dart
@@ -1,4 +1,8 @@
+import 'package:audio_player/controllers/audio_controller.dart';
+import 'package:audio_player/utils/global_helpers.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:on_audio_query/on_audio_query.dart';
 
 class AudioListPage extends StatelessWidget {
   const AudioListPage({super.key});
@@ -6,6 +10,34 @@ class AudioListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('All Songs'),
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Get.back();
+          },
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: AudioController.to.audios.length,
+        itemBuilder: (context, index) {
+          final audio = AudioController.to.audios[index];
+          return ListTile(
+            leading: QueryArtworkWidget(
+              // controller: _audioQuery,
+              id: audio.id,
+              type: ArtworkType.AUDIO,
+            ),
+            title: Text(audio.displayName),
+            onTap: () {
+              loggerDebug(audio, 'song model');
+            },
+          );
+        },
+      ),
+    );
   }
 }
